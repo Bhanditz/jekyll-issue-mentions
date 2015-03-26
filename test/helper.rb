bd9dbfa6eb3137969efe1e2f12603e195eaf528a
Harish Shetty <harish@workato.com>
2015-03-26 10:44:42 -0700

    Update helper.rb

--- test/helper.rb
+++ test/helper.rb
@@ -1,8 +1,6 @@
 require 'rubygems'
 require 'minitest/autorun'
 require 'shoulda'
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
